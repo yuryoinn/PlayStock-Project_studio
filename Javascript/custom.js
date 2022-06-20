a2d7a9baf0d2da58c1b1fcbb05a23fd61d920749
yuryoinn <yuryoinn88@gmail.com>
2022-06-21 01:01:50 +0900

    sd
    sd

--- Javascript/custom.js
+++ Javascript/custom.js
@@ -1,119 +1,120 @@
 $(function() {
 
-    /* Slick Slider - Welcome */
+  /* Slick Slider - Welcome */
 
-    $('.slideshow').slick({
-        infinite: true,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true,
-        speed: 1000,
-        pauseOnHover: false 
-    });
-
-    /* Slick Slider - Mock-up-slider */
-
-    $('.mockup-slider').slick({
+  $('.slideshow').slick({
       infinite: true,
       dots: false,
       autoplay: true,
-      autoplaySpeed: 1800
+      autoplaySpeed: 2000,
+      fade: true,
+      speed: 1000,
+      pauseOnHover: false 
   });
 
-    /* Slick Slider - Review */
+  /* Slick Slider - Mock-up-slider */
 
-    $('.review-slider').slick({
-        infinite: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1700
-    });
+  $('.mockup-slider').slick({
+    infinite: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1800
+});
 
-    /* TypeIn - Welcome */
+  /* Slick Slider - Review */
 
-    $('#typing').typeIt({
-        strings: ["기본 개념 튜토리얼을.", "퀴즈를 통해 레벨업.", "모의투자로 마무리."], // 타이핑 텍스트 입력
-        speed: 130, // 알파벳 타이핑 속도
-        autoStart: true, // 자동 재생 사용
-        breakLines: false, // 줄 바꿈 사용안함
-        loop: true
-    });
+  $('.review-slider').slick({
+      infinite: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1700
+  });
 
-    /* AOS Scroll Reveal Animation */
+  /* TypeIn - Welcome */
 
-    AOS.init({
-      offset: 200,
-      delay: 0,
-      duration: 1000,
-      easing: 'ease',
-      once: false,
-      mirror: true,
-      anchorPlacement: 'top-bottom'
-    });    
+  $('#typing').typeIt({
+      strings: ["기본 개념 튜토리얼을.", "퀴즈를 통해 레벨업.", "모의투자로 마무리."], // 타이핑 텍스트 입력
+      speed: 130, // 알파벳 타이핑 속도
+      autoStart: true, // 자동 재생 사용
+      breakLines: false, // 줄 바꿈 사용안함
+      loop: true
+  });
 
-    AOS.init();
+  /* AOS Scroll Reveal Animation */
 
+  AOS.init({
+    offset: 200,
+    delay: 0,
+    duration: 1000,
+    easing: 'ease',
+    once: false,
+    mirror: true,
+    anchorPlacement: 'top-bottom'
+  });    
+
+  AOS.init();
+
+  
 })
 
 $(function(){
 
-    /* Header Scroll Change */
+  /* Header Scroll Change */
 
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 300) {
-        $('header').addClass('active')
-      }
-      else {
-        $('header').removeClass('active')
-      }
-    })
-
-    /* Scroll Top */
-
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 50) {
-        $('.btn-top').addClass('active')
-      }
-      else {
-        $('.btn-top').removeClass('active')  
-      }
-    })
-    
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 300) {
+      $('header').addClass('active')
+    }
+    else {
+      $('header').removeClass('active')
+    }
   })
 
-  /* Custom Mouse */
+  /* Scroll Top */
 
-  let cursorItem;
-  let circle;
-  let x=0, y = 0;
-  let mx=0, my = 0;
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 50) {
+      $('.btn-top').addClass('active')
+    }
+    else {
+      $('.btn-top').removeClass('active')  
+    }
+  })
+  
+})
 
-  window.onload = function(){
-    
-    cursorItem = document.querySelector(".cursorItem");
-    circle = cursorItem.querySelector(".circle");    
+/* Custom Mouse */
 
-    window.addEventListener("mousemove", function(e){
-        x = e.clientX;
-        y = e.clientY;
-        cursorItem.style.transform = "translate("+ x +"px, "+ y + "px )";
-    });
-    
-    loop();
+let cursorItem;
+let circle;
+let x=0, y = 0;
+let mx=0, my = 0;
 
-    /* Progressive Bar */
+window.onload = function(){
+  
+  cursorItem = document.querySelector(".cursorItem");
+  circle = cursorItem.querySelector(".circle");    
 
-    bar = document.getElementsByClassName("bar")[0];
-    
+  window.addEventListener("mousemove", function(e){
+      x = e.clientX;
+      y = e.clientY;
+      cursorItem.style.transform = "translate("+ x +"px, "+ y + "px )";
+  });
+  
+  loop();
+
+  /* Progressive Bar */
+
+  bar = document.getElementsByClassName("bar")[0];
+  
 }
 
 function loop(){
-    mx += (x - mx ) * .09;
-    my += (y - my ) * .09; 
-    cursorItem.style.transform = "translate("+ mx +"px, "+ my + "px )";
+  mx += (x - mx ) * .09;
+  my += (y - my ) * .09; 
+  cursorItem.style.transform = "translate("+ mx +"px, "+ my + "px )";
 
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 /* Progressive Bar */
@@ -123,12 +124,30 @@ let bar;
 
 
 window.addEventListener("scroll", function(e){
-    scrollTop = document.documentElement.scrollTop;
+  scrollTop = document.documentElement.scrollTop;
 
-    // 가로 퍼센트값 = 현재 스크롤 탑 위치 / (문서 전체 길이 - 윈도우 창 높이 ) * 100;
-    let per = Math.ceil(scrollTop / (document.body.scrollHeight - window.outerHeight) * 100);
-    console.log(per);
+  // 가로 퍼센트값 = 현재 스크롤 탑 위치 / (문서 전체 길이 - 윈도우 창 높이 ) * 100;
+  let per = Math.ceil(scrollTop / (document.body.scrollHeight - window.outerHeight) * 100);
+  console.log(per);
 
-    bar.style.width = per + "%";
+  bar.style.width = per + "%";
 
 }, false)
+
+/* Setinterval typing */
+
+document.addEventListener("DOMContentLoaded", function (){
+  const content = "Design Concept";
+  const text = document.querySelector(".typing-txt");
+  let i = 0;
+
+  function typing(){
+      let txt = content[i++];
+      text.innerHTML += txt;
+      if (i > content.length) {
+          text.textContent = "";
+          i = 0;
+      }
+  }
+  setInterval(typing, 200)  
+})
